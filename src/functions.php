79e863d3ca2a25c7c8d0a55297f46bf4d198f137
tangwei <tw2066@163.com>
2024-06-27 14:37:01 +0800

    fix code

--- src/functions.php
+++ src/functions.php
@@ -1,14 +1,15 @@
 <?php
 
-namespace Tinderbox\ClickhouseBuilder;
+declare(strict_types=1);
 
+namespace Tinderbox\ClickhouseBuilder;
 
 use Tinderbox\ClickhouseBuilder\Query\Expression;
 
 /**
  * Call the given Closure with the given value then return the value.
  *
- * @param mixed    $value
+ * @param mixed $value
  * @param callable $callback
  *
  * @return mixed
@@ -19,35 +20,29 @@ function tp($value, $callback)
 
     return $value;
 }
-    /**
-     * Flatten a multi-dimensional array into a single level.
-     *
-     * @param array $array
-     * @param int   $depth
-     *
-     * @return array
-     */
-    function array_flatten($array, $depth = INF): array
-    {
-        return array_reduce($array, function ($result, $item) use ($depth) {
-            if (!is_array($item)) {
-                return array_merge($result, [$item]);
-            } elseif ($depth === 1) {
-                return array_merge($result, array_values($item));
-            } else {
-                return array_merge($result, array_flatten($item, $depth - 1));
-            }
-        }, []);
-    }
+/**
+ * Flatten a multi-dimensional array into a single level.
+ *
+ * @param array $array
+ * @param int $depth
+ */
+function array_flatten($array, $depth = INF): array
+{
+    return array_reduce($array, function ($result, $item) use ($depth) {
+        if (! is_array($item)) {
+            return array_merge($result, [$item]);
+        }
+        if ($depth === 1) {
+            return array_merge($result, array_values($item));
+        }
+        return array_merge($result, array_flatten($item, $depth - 1));
+    }, []);
+}
 
-    /**
-     * Wrap string into Expression object for inserting in sql query as is.
-     *
-     * @param string $expr
-     *
-     * @return Expression
-     */
-    function raw(string $expr): Expression
-    {
-        return new Expression($expr);
-    }
+/**
+ * Wrap string into Expression object for inserting in sql query as is.
+ */
+function raw(string $expr): Expression
+{
+    return new Expression($expr);
+}
